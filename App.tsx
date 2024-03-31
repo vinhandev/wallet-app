ae499ca9981e4d81969900a11c4d65aa6c08ea3f
vinhandev <vinhan.dev@gmail.com>
2024-03-31 17:03:41 +0700

    Pan Gesture

--- App.tsx
+++ App.tsx
@@ -1,7 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import Home from '~/screens/Home/Home';
 
 export default function App() {
-  return <Home />;
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <Home />
+    </GestureHandlerRootView>
+  );
 }
--- src/components/CardList/CardList.tsx
+++ src/components/CardList/CardList.tsx
@@ -1,5 +1,5 @@
 import { Image, Text, View } from 'react-native';
-
+import { Gesture, GestureDetector } from 'react-native-gesture-handler';
 const cards = [
   require('~/assets/images/cards/Card 1.png'),
   require('~/assets/images/cards/Card 2.png'),
@@ -12,20 +12,33 @@ const cards = [
   require('~/assets/images/cards/Card 9.png'),
 ];
 export function CardList() {
+  const pan = Gesture.Pan()
+    .onStart(() => {
+      console.log('start');
+    })
+    .onChange((e) => {
+      console.log('change, y:', e.changeY);
+    })
+    .onEnd(() => {
+      console.log('end');
+    });
+
   return (
-    <View style={{ padding: 10 }}>
-      {cards.map((card, index) => (
-        <Image
-          key={index}
-          source={card}
-          style={{
-            width: '100%',
-            height: undefined,
-            aspectRatio: 7 / 4,
-            marginVertical: 5,
-          }}
-        />
-      ))}
-    </View>
+    <GestureDetector gesture={pan}>
+      <View style={{ padding: 10 }}>
+        {cards.map((card, index) => (
+          <Image
+            key={index}
+            source={card}
+            style={{
+              width: '100%',
+              height: undefined,
+              aspectRatio: 7 / 4,
+              marginVertical: 5,
+            }}
+          />
+        ))}
+      </View>
+    </GestureDetector>
   );
 }
